57fd7482d05d826599c1839163c22a13075d9ace
raieegulhane <gulhaneraiee17@gmail.com>
2022-06-21 18:05:06 +0530

    added routes

---
+++ src/routes/site-routes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Mockman from 'mockman-js';
+import { Home, Private1, Private2, Private3, Login, Signup, NotFound } from "../pages";
+
+export const SiteRoutes = () => {
+    return(
+        <Routes>
+            <Route path="/mockman" element={<Mockman />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/private1" element={<Private1 />} />
+            <Route path="/private2" element={<Private2 />} />
+            <Route path="/private3" element={<Private3 />} />
+        </Routes>
+    );
+}
