4214081a0a533e4c264809272af174c274a556bb
raieegulhane <gulhaneraiee17@gmail.com>
2022-06-21 18:03:49 +0530

    cleanup

--- src/server.js
+++ src/server.js
@@ -1,27 +1,11 @@
 import { Server, Model, RestSerializer } from "miragejs";
-import {
-  deleteFromArchivesHandler,
-  getAllArchivedNotesHandler,
-  restoreFromArchivesHandler,
-} from "./backend/controllers/ArchiveController";
+
 import {
   loginHandler,
   signupHandler,
   userProfilehandler,
 } from "./backend/controllers/AuthController";
-import {
-  archiveNoteHandler,
-  createNoteHandler,
-  deleteNoteHandler,
-  getAllNotesHandler,
-  trashNoteHandler,
-  updateNoteHandler,
-} from "./backend/controllers/NotesController";
-import {
-  deleteFromTrashHandler,
-  getAllTrashNotesHandler,
-  restoreFromTrashHandler,
-} from "./backend/controllers/TrashController";
+
 import { users } from "./backend/db/users";
 
 export function makeServer({ environment = "development" } = {}) {
@@ -30,7 +14,6 @@ export function makeServer({ environment = "development" } = {}) {
       application: RestSerializer,
     },
     environment,
-    // TODO: Use Relationships to have named relational Data
     models: {
       user: Model,
       notes: Model,
@@ -57,29 +40,6 @@ export function makeServer({ environment = "development" } = {}) {
 
       // user route (private)
       this.get("/user", userProfilehandler.bind(this));
-
-      // notes routes (private)
-      this.get("/notes", getAllNotesHandler.bind(this));
-      this.post("/notes", createNoteHandler.bind(this));
-      this.post("/notes/:noteId", updateNoteHandler.bind(this));
-      this.delete("/notes/:noteId", deleteNoteHandler.bind(this));
-      this.post("/notes/archives/:noteId", archiveNoteHandler.bind(this));
-      this.post("/notes/trash/:noteId", trashNoteHandler.bind(this));
-
-      // archive routes (private)
-      this.get("/archives", getAllArchivedNotesHandler.bind(this));
-      this.post(
-        "/archives/restore/:noteId",
-        restoreFromArchivesHandler.bind(this)
-      );
-      this.delete(
-        "/archives/delete/:noteId",
-        deleteFromArchivesHandler.bind(this)
-      );
-      // trash routes (private)
-      this.get("/trash", getAllTrashNotesHandler.bind(this));
-      this.post("/trash/restore/:noteId", restoreFromTrashHandler.bind(this));
-      this.delete("/trash/delete/:noteId", deleteFromTrashHandler.bind(this));
     },
   });
   return server;
