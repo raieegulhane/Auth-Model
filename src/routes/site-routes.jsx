5e5075936a203b735420fffd9526db0a1fc689fa
raieegulhane <gulhaneraiee17@gmail.com>
2022-06-28 12:51:35 +0530

    routes

--- src/routes/site-routes.jsx
+++ src/routes/site-routes.jsx
@@ -5,15 +5,15 @@ import { Home, Private1, Private2, Private3, Login, Signup, NotFound } from "../
 export const SiteRoutes = () => {
     return(
         <Routes>
-            <Route path="/mockman" element={<Mockman />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/private1" element={<Private1 />} />
-            <Route path="/private2" element={<Private2 />} />
-            <Route path="/private3" element={<Private3 />} />
+            <Route exact path="/mockman" element={<Mockman />} />
+            <Route exact path="/Login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="*" element={<NotFound />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/private1" element={<Private1 />} />
+            <Route exact path="/private2" element={<Private2 />} />
+            <Route exact path="/private3" element={<Private3 />} />
         </Routes>
     );
 }
